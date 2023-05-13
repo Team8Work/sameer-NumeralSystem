ae2608c050e3e9ac9a9e482dfb95afacccd93262
Sameers1 <m.sameersabirbutt@gmail.com>
2023-05-13 21:33:05 +0500

    First version 1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿    static void Main(string[] args)
+        {
+            Console.Write("Enter a Number : ");
+            int n = int.Parse(Console.ReadLine());
+            
+            string r = string.Empty;
+            for (int i = 0; n > 2; i++)
+            {
+                r = n % 2 + r;
+                n = n / 2;
+            }
+            Console.WriteLine("Binary Namber Is : "+ r);
+            
+            Console.ReadKey();
+        }
+   
